b6e61a37a6a3dbfd06065f642678fc03cc369e46
Ranadeep Polavarapu <ranadeep@icloud.com>
2015-02-09 15:28:57 -0500

    Added MySQL binding

--- src/backend/inventory/inventory/settings.py
+++ src/backend/inventory/inventory/settings.py
@@ -58,8 +58,8 @@ WSGI_APPLICATION = 'inventory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mysql.connector.django',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), # Change this
     }
 }
 
