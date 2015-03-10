35a97fa8131e9682e8dab5e5a7f8fbbb6b32deb0
Ranadeep Polavarapu <ranadeep@icloud.com>
2015-03-10 15:44:31 -0400

    interface.py: Added docs for SQL block.

--- src/frontend/interface.py
+++ src/frontend/interface.py
@@ -18,6 +18,7 @@ except ImportError as e:
 # dP   `" dP   Yb 88Yb88 `Ybo."   88     dPYb   88Yb88   88   `Ybo."
 # Yb      Yb   dP 88 Y88 o.`Y8b   88    dP__Yb  88 Y88   88   o.`Y8b
 #  YboodP  YbodP  88  Y8 8bodP'   88   dP""""Yb 88  Y8   88   8bodP'
+
 DB_HOST = "104.236.198.237"
 DB_USER = "pola3120"
 DB_PASSWD = "D3xBfwaebmTRJm6S"
@@ -43,11 +44,10 @@ def clear_tty():
         os.system("clear")
     return
 
-# .dP"Y8  dP"Yb  88         888888 88   88 88b 88  dP""b8 888888 88  dP"Yb  88b 88 .dP"Y8
-# `Ybo." dP   Yb 88         88__   88   88 88Yb88 dP   `"   88   88 dP   Yb 88Yb88 `Ybo."
-# o.`Y8b Yb b dP 88  .o     88""   Y8   8P 88 Y88 Yb        88   88 Yb   dP 88 Y88 o.`Y8b
-# 8bodP'  `"YoYo 88ood8     88     `YbodP' 88  Y8  YboodP   88   88  YbodP
-# 88  Y8 8bodP'
+# .dP"Y8  dP"Yb  88         888888 88   88 88b 88  dP""b8 888888 88  dP"Yb  88b 88 .dP"Y8 
+# `Ybo." dP   Yb 88         88__   88   88 88Yb88 dP   `"   88   88 dP   Yb 88Yb88 `Ybo." 
+# o.`Y8b Yb b dP 88  .o     88""   Y8   8P 88 Y88 Yb        88   88 Yb   dP 88 Y88 o.`Y8b 
+# 8bodP'  `"YoYo 88ood8     88     `YbodP' 88  Y8  YboodP   88   88  YbodP  88  Y8 8bodP' 
 
 
 def initalize_DB(host, user, password, db_name):
